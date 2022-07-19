54420e18f6170e9f8ec22886fe025de2e01abb48
Viet Anh Le <mc.vietanh@gmail.com>
2022-07-18 20:43:06 -0400

    setup be server

--- index.js
+++ index.js
@@ -1,54 +1,77 @@
-const express = require('express')
-const pg = require('pg')
+require('dotenv').config();
+const express = require('express');
+const pg = require('pg');
 
-const app = express()
+const app = express();
 // configs come from standard PostgreSQL env vars
 // https://www.postgresql.org/docs/9.6/static/libpq-envars.html
-const pool = new pg.Pool()
+const pool = new pg.Pool();
 
 const queryHandler = (req, res, next) => {
-  pool.query(req.sqlQuery).then((r) => {
-    return res.json(r.rows || [])
-  }).catch(next)
-}
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  pool
+    .query(req.sqlQuery)
+    .then((r) => {
+      return res.json(r.rows || []);
+    })
+    .catch(next);
+};
 
 app.get('/', (req, res) => {
-  res.send('Welcome to EQ Works 😎')
-})
+  res.send('Welcome to EQ Works 😎');
+});
 
-app.get('/events/hourly', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  '/events/hourly',
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date, hour, events
     FROM public.hourly_events
     ORDER BY date, hour
     LIMIT 168;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/events/daily', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  '/events/daily',
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date, SUM(events) AS events
     FROM public.hourly_events
     GROUP BY date
     ORDER BY date
     LIMIT 7;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/stats/hourly', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  '/stats/hourly',
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date, hour, impressions, clicks, revenue
     FROM public.hourly_stats
     ORDER BY date, hour
     LIMIT 168;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/stats/daily', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  '/stats/daily',
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date,
         SUM(impressions) AS impressions,
         SUM(clicks) AS clicks,
@@ -57,33 +80,39 @@ app.get('/stats/daily', (req, res, next) => {
     GROUP BY date
     ORDER BY date
     LIMIT 7;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/poi', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  '/poi',
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT *
     FROM public.poi;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
 app.listen(process.env.PORT || 5555, (err) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   } else {
-    console.log(`Running on ${process.env.PORT || 5555}`)
+    console.log(`Running on ${process.env.PORT || 5555}`);
   }
-})
+});
 
 // last resorts
 process.on('uncaughtException', (err) => {
-  console.log(`Caught exception: ${err}`)
-  process.exit(1)
-})
+  console.log(`Caught exception: ${err}`);
+  process.exit(1);
+});
 process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
-  process.exit(1)
-})
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  process.exit(1);
+});
